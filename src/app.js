a30948613de9c3e6e4daac52067c80e4d47490c9
meriy100 <ttattataa@gmail.com>
2022-11-13 12:53:25 +0900

    [Remove] all

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import {Elm} from "./Main.elm";
-import './styles/style.scss';
-
-const contents = require('./contents/ja.yml');
-console.log(contents);
-
-const storageKey = "store";
-const flags = { storeKey: localStorage.getItem(storageKey), contents: contents };
-const app = Elm.Main.init({flags: JSON.stringify(flags)});
